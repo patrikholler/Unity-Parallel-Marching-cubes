82e3db9da488650896c36262273a809583d8cfd3
Patrik Holler <pstarhu@gmail.com>
2022-08-08 20:31:19 +0200

    init

---
+++ Assets/Scenes/Job/MeshBuilderJob.cs
@@ -0,0 +1,40 @@
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace VoxelEngine
+{
+    public struct MeshBuilderJob : IJob
+    {
+        [ReadOnly]
+        public NativeArray<VertexData> vertexData;
+
+        [ReadOnly]
+        public NativeArray<ushort> indices;
+
+        public Mesh.MeshDataArray _meshDataArray;
+
+        public void Execute()
+        {
+            if (indices.Length > 0)
+            {
+                Mesh.MeshData meshData = _meshDataArray[0];
+                meshData.SetIndexBufferParams(indices.Length, IndexFormat.UInt16);
+                meshData.SetVertexBufferParams(vertexData.Length, vertexAttributeDescriptor);
+                meshData.GetIndexData<ushort>().CopyFrom(indices);
+                meshData.GetVertexData<VertexData>().CopyFrom(vertexData);
+                meshData.subMeshCount = 1;
+                meshData.SetSubMesh(0, new SubMeshDescriptor(0, indices.Length));
+            }
+        }
+
+        private static readonly VertexAttributeDescriptor[] vertexAttributeDescriptor =
+        {
+            new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, dimension: 3, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, dimension: 3, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.Color, VertexAttributeFormat.Float32, dimension: 4, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, dimension: 4, stream: 0)
+        };
+    }
+}
---
+++ Assets/Scenes/Job/VoxelManager.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine.Rendering;
+
+namespace VoxelEngine
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public class VoxelManager : MonoBehaviour
+    {
+        private Mesh mesh;
+        private MeshFilter meshFilter;
+        private MeshCollider meshCollider;
+        private int3 gridSize;
+        private JobHandle marchingCubesJobHandle;
+        private JobHandle meshBuilderHandle;
+
+        private float isoLevel = 0.5f;
+
+        [SerializeField] 
+        private int3 chunkSize = new int3(16, 16, 16);
+
+        [SerializeField]
+        private float noiseScale = 0.5f;
+
+        [Range(0.0f,0.5f)]
+        [SerializeField]
+        private float noiseSpeed = 0.00005f;
+
+        bool growEffect;
+
+        void Awake()
+        {
+            mesh = new Mesh();
+            mesh.indexFormat = IndexFormat.UInt16;
+            mesh.name = "Tetrahedron";
+            meshFilter = GetComponent<MeshFilter>();
+            meshCollider = GetComponent<MeshCollider>();
+            gridSize = chunkSize + new int3(1,1,1);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GenerateVoxel();
+        }
+
+        private void Update()
+        {
+            if (marchingCubesJobHandle.IsCompleted)
+            {
+                if (!growEffect)
+                {
+                    if (noiseScale >= (chunkSize.x / 2 - 0.5f)) growEffect = true;
+                    noiseScale += noiseSpeed;
+                }
+
+                if (growEffect)
+                {
+                    if (noiseScale <= 0f) growEffect = false;
+                    noiseScale -= noiseSpeed;
+                }
+
+                GenerateVoxel();
+            }
+        }
+
+        void GenerateVoxel()
+        {
+            NativeArray<VertexData> nativeVertexData = new NativeArray<VertexData>(15 * gridSize.x * gridSize.y * gridSize.z, Allocator.TempJob);
+            NativeArray<ushort> nativeIndicess = new NativeArray<ushort>(15 * gridSize.x * gridSize.y * gridSize.z, Allocator.TempJob);
+            NativeCounter nativCounter = new NativeCounter(Allocator.TempJob);
+
+            MarchingCubesJob marchingcubesJob = new MarchingCubesJob
+            {
+                isoLevel = isoLevel,
+                noiseScale = noiseScale,
+                chunkSize = chunkSize,
+                gridSize = gridSize,
+                indices = nativeIndicess,
+                vertexData = nativeVertexData,
+                vertexCounter = nativCounter,
+            };
+
+            marchingCubesJobHandle = marchingcubesJob.Schedule(gridSize.x * gridSize.y * gridSize.z, 32);
+            marchingCubesJobHandle.Complete();
+
+            if (marchingCubesJobHandle.IsCompleted)
+            {
+                Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
+
+                MeshBuilderJob meshBuilderJob = new MeshBuilderJob()
+                {
+                    indices = marchingcubesJob.indices, // HERE WE STRAIGHT USE THE DATA OF THE MESH GENERATOR
+                    vertexData = marchingcubesJob.vertexData, // SAME HERE
+                    _meshDataArray = meshDataArray, // MESH ALLOCATOR
+                };
+
+                meshBuilderHandle = meshBuilderJob.Schedule(marchingCubesJobHandle);
+                meshBuilderHandle.Complete();
+
+                mesh.Clear();
+
+                Mesh.ApplyAndDisposeWritableMeshData(meshBuilderJob._meshDataArray, mesh, MeshUpdateFlags.DontValidateIndices | MeshUpdateFlags.DontNotifyMeshUsers);
+
+                meshFilter.sharedMesh = mesh;
+                //meshCollider.sharedMesh = mesh;
+            }
+
+            nativCounter.Dispose();
+            nativeIndicess.Dispose();
+            nativeVertexData.Dispose();
+        }
+
+        static readonly VertexAttributeDescriptor[] vertexAttributeDescriptor =
+        {
+            new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, dimension: 3, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, dimension: 3, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.Color, VertexAttributeFormat.Float32, dimension: 4, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, dimension: 4, stream: 0)
+        };
+    }
+
+    public struct VertexData
+    {
+        public float3 Position;
+        public float3 Normal;
+        public Color Color;
+        public float4 UV1;
+    }
+}
